861a775a873e86d56a8377d36e134f5a23c1ac1a
Stas <stas.schnitzer@gmail.com>
2025-07-18 01:07:53 -0400

    Fixed scene set up
    All objects are zeroed and easy to work with for level design
    
    Added Gizmo script
    put on any object to show its BoxColider2D

---
+++ Assets/scripts/Gizmos/ShowBoxCollider2DGizmo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ShowBoxCollider2DGizmo : MonoBehaviour
+{
+    private BoxCollider2D col;
+
+    private void Awake()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (col == null) col = GetComponent<BoxCollider2D>();
+        Gizmos.color = Color.cyan;
+        // Draw a wireframe box at the collider’s bounds
+        Vector2 pos = (Vector2)transform.position + col.offset;
+        Gizmos.DrawWireCube(pos, col.size);
+    }
+}

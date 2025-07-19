413ba2bf5b38039ae32a6889c54ee732f99f240d
Qing <qingx.game37@gmail.com>
2025-07-19 14:02:59 -0700

    small changes, still in progress

--- Assets/scripts/platformTypes/breakPlatform.cs
+++ Assets/scripts/platformTypes/breakPlatform.cs
@@ -9,30 +9,67 @@ public class breakPlatform : MonoBehaviour
 
     [Header("Breakable Platform Settings")]
     private Vector3 originalPosition;
+    [SerializeField] private float shakeTime = 0.5f;
+    [SerializeField] private float origShakeTime = 0.5f;
+    private float shakeMagnitude = 0.05f;
+    private bool platformActive = false;
+    private float cooldown = 5f;
+    private bool reset = false;
 
-    [SerializeField] private float popUpHeight = 0.3f;
-
-    private Rigidbody2D platform;
+    private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        platform = gameObject.GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
         player = Player_Manager.Instance.player.transform;
         originalPosition = transform.position;
+        Instantiate(rb, originalPosition, Quaternion.identity);
     }
-    
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
 
-        if (collision.gameObject.CompareTag("Player"))
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !platformActive)
         {
-            Debug.Log("FKLDSJFKLJSDKLFJDL");
+            platformActive = true;
+            Debug.Log("KFJSDLKFJKLDSJFKLDS");
+        }
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            Destroy(gameObject);    // destroys on any collision
+            reset = true;
         }
     }
 
     void Update()
     {
-        
+        if (platformActive)
+        {
+            // shake the platform
+            if (shakeTime > 0)
+            {
+                transform.position = originalPosition + new Vector3(Random.Range(-shakeMagnitude, shakeMagnitude), Random.Range(-shakeMagnitude, shakeMagnitude), 0);
+                shakeTime -= Time.deltaTime;
+            }
+            // drop the platform
+            else
+            {
+                rb.bodyType = RigidbodyType2D.Dynamic;
+                rb.gravityScale = 1f;
+            }
+        }
+        if (reset)
+        {
+            cooldown -= Time.deltaTime;
+            if (cooldown < 0)
+            {
+                cooldown = 5f;
+                platformActive = false;
+                shakeTime = origShakeTime;
+                Instantiate(rb, originalPosition, Quaternion.identity);
+                reset = false;
+            }
+        }
     }
 }

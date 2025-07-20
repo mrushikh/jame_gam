d8baf823821a8cc688395ae13bd9ad6c822b929d
mrushikh <m.rushikh@gmail.com>
2025-07-20 01:41:56 -0400

    mech animals

--- Assets/scripts/smallObjects.cs
+++ Assets/scripts/smallObjects.cs
@@ -1,14 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class smallObjects : MonoBehaviour
 {
+    private IEnumerator destroyNut()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+    private void Start()
+    {
+        StartCoroutine(destroyNut());
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Player_Manager.Instance.RespawnPlayer();
+            Destroy(gameObject);
         }
-        Destroy(gameObject);    // destroys on any collision
+           // destroys on any collision
     }
 }
